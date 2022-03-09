3d63a249ca91e2bc6a5fcef8a67d7e260472cb5c
sheyzi <gistkiosk@gmail.com>
2022-03-09 15:56:57 +0100

    Addded get, update and delete routes for items!

--- repositories/items.py
+++ repositories/items.py
@@ -2,10 +2,13 @@ from typing import List
 from typing import Union
 
 from fastapi import HTTPException
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from models.items import Items
 from schemas.items import ItemCreate
+from schemas.items import ItemUpdate
+from utils.sanitize_schemas_body import sanitize
 
 
 def create(body: ItemCreate, db: Session) -> Items:
@@ -17,7 +20,9 @@ def create(body: ItemCreate, db: Session) -> Items:
 
 
 def get_all(db: Session, limit: int = 20, skip: int = 0) -> List[Items]:
-    items = db.query(Items).order_by(Items.created_at).offset(skip).limit(limit).all()
+    items = (
+        db.query(Items).order_by(desc(Items.created_at)).offset(skip).limit(limit).all()
+    )
     return items
 
 
@@ -43,3 +48,19 @@ def get_with_slug(db: Session, slug: str) -> Items:
 def get_with_slug_or_none(db: Session, slug: str) -> Union[Items, None]:
     item = db.query(Items).filter(Items.slug == slug).first()
     return item
+
+
+def update_with_slug(db: Session, slug: str, body: ItemUpdate) -> Items:
+    item = get_with_slug(db, slug)
+    filtered_body = sanitize(body)
+    db.query(Items).filter(Items.slug == slug).update(filtered_body)
+    db.commit()
+    db.refresh(item)
+    return item
+
+
+def delete_with_slug(db: Session, slug: str):
+    item = get_with_slug(db, slug)
+    db.delete(item)
+    db.commit()
+    return True
--- schemas/items.py
+++ schemas/items.py
@@ -14,7 +14,31 @@ class ItemBase(BaseModel):
 
 
 class ItemCreate(ItemBase):
-    pass
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Pizza",
+                "cost": 5,
+                "price": 15,
+                "discount": 10,
+                "inventory": 100,
+                "available": True,
+            }
+        }
+
+
+class ItemUpdate(ItemBase):
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Pizza",
+                "cost": 5,
+                "price": 15,
+                "discount": 10,
+                "inventory": 100,
+                "available": True,
+            }
+        }
 
 
 class ItemOut(ItemBase):
@@ -23,4 +47,17 @@ class ItemOut(ItemBase):
     created_at: datetime
 
     class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "pizza",
+                "slug": "pizza-53627",
+                "cost": 5,
+                "price": 15,
+                "discount": 10,
+                "inventory": 100,
+                "available": True,
+                "created_at": "2022-03-09T14:27:04.800Z",
+            }
+        }
         orm_mode = True
