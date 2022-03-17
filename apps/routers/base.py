67a783fcd7f91dba4463852454b54538bd2268d5
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-03-17 22:44:25 +0100

    Created user routes

--- apps/routers/base.py
+++ apps/routers/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from apps.routers.product.product import router as items_router
+from apps.routers.user.user import router as user_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(items_router)
+router.include_router(user_router)
